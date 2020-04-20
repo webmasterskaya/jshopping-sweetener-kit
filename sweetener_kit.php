0e3e78df353d7fd7793aeade16d2eeaad4fe654d
Artem Vasilev <kern.usr@gmail.com>
2020-04-20 19:02:22 +0300

    Добавил копирайты

--- sweetener_kit.php
+++ sweetener_kit.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * @package   JShopping - Sweetener kit
+ * @version    __DEPLOY_VERSION__
+ * @author    Artem Vasilev - Webmasterskaya
+ * @copyright  Copyright (c) 2020 Webmasterskaya. All rights reserved.
+ * @license    GNU General Public License version 3 or later; see LICENSE.txt
+ * @link       https://webmasterskaya.xyz/
+ */
 
 defined('_JEXEC') or die;
 
